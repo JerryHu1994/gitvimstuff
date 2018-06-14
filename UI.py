38a2faef641fe13b75491e619eb93729140935f0
Jieru Hu <hjr01211@gmail.com>
2018-06-13 23:29:03 -0500

    Initial Commit

---
+++ UI.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+##################################
+# University of Wisconsin-Madison
+# Author: Jieru Hu
+##################################
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# main method
+def main():
+
+
+if __name__ == "__main__":
+  main()
+
